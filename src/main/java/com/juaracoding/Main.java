6864995ed09513b1c088a1ca7af1f7c99298ed3c
koswara-dev <53398431+koswara-dev@users.noreply.github.com>
2023-12-04 19:28:43 +0700

    Update Main.java

--- src/main/java/com/juaracoding/Main.java
+++ src/main/java/com/juaracoding/Main.java
@@ -3,5 +3,6 @@ package com.juaracoding;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Indonesia");
     }
 }
